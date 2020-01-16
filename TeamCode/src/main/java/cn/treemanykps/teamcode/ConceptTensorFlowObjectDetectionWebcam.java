a1bc3a973068a0600dbe426042ed97d011f8caf1
Bill Wu <beellyy2003@outlook.com>
2020-01-16 17:21:20 +0800

    Back Up

--- TeamCode/src/main/java/cn/treemanykps/teamcode/ConceptTensorFlowObjectDetectionWebcam.java
+++ TeamCode/src/main/java/cn/treemanykps/teamcode/ConceptTensorFlowObjectDetectionWebcam.java
@@ -51,7 +51,7 @@ import java.util.List;
  * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
  * is explained below.
  */
-@TeleOp(name = "TensorFlow Object Detection Webcam", group = "Concept")
+@TeleOp(name = "TFOD Test", group = "Concept")
 //@Disabled
 public class ConceptTensorFlowObjectDetectionWebcam extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
